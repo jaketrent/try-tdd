e4f99bbbfc985d84c68ec3ac312a3f03ab520cb4
jaketrent <trent.jake@gmail.com>
2014-05-13 08:09:13 -0600

    Initial commit

---
+++ app/combine.js
@@ -0,0 +1,21 @@
+/**
+ * Transforms an array of objects into a single object.
+ *
+ * Sample input:
+ *
+ * [{ 'course-id': 123 }, { title: 'Dev Mountain' }, { start_date: new Date(2014, 2, 14) }]
+ *
+ * Sample output:
+ *
+ * {
+ *   'course-id': 123,
+ *   title: 'Dev Mountain',
+ *   start_date: new Date(2014, 2, 14)
+ * }
+ *
+ * @param {Array} fields array of single-property objects
+ * @returns {Object} object with all fields included
+ */
+module.exports = function (fields) {
+
+}
---
+++ test/unit/combine.spec.js
@@ -0,0 +1,10 @@
+var should = require('should')
+var combine = require('../../app/combine')
+
+describe('combine', function () {
+
+  it('exists', function () {
+    should.exist(combine)
+  })
+
+})
